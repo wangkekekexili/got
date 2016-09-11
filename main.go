099385d6cc400f5248f8b7b47faff4e9a57e7b91
Ke Wang <ke@squareup.com>
2016-09-11 13:05:38 -0400

    got completely rewrite.

--- main.go
+++ main.go
@@ -2,57 +2,131 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
 	"strings"
+
+	"github.com/mitchellh/go-homedir"
 )
 
+const (
+	gitignoreFilesDirName = ".got"
+	gitignoreRepoURL      = "https://api.github.com/repos/github/gitignore/tarball"
+)
+
+func help() {
+	fmt.Println("Usage: got generate <name1> <name2> ...")
+}
+
+func getIgnores() (map[string]string, error) {
+	// Check if .got folder exists in the home holder.
+	// If it doens't, download from github gitignore repository.
+	homeDir, err := homedir.Dir()
+	if err != nil {
+		return nil, err
+	}
+	ignoreFilesDir := path.Join(homeDir, gitignoreFilesDirName)
+	_, err = os.Stat(ignoreFilesDir)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return nil, err
+		}
+
+		// Create the directory.
+		if err = os.Mkdir(ignoreFilesDir, 0700); err != nil {
+			return nil, err
+		}
+
+		// Create the archive file.
+		archiveFilename := path.Join(ignoreFilesDir, "archive.tar.gz")
+		var archiveFile *os.File
+		archiveFile, err = os.Create(archiveFilename)
+		if err != nil {
+			return nil, err
+		}
+		defer archiveFile.Close()
+
+		// Download the archive file.
+		var getResp *http.Response
+		getResp, err = http.Get(gitignoreRepoURL)
+		if err != nil {
+			return nil, err
+		}
+		defer getResp.Body.Close()
+
+		if _, err = io.Copy(archiveFile, getResp.Body); err != nil {
+			return nil, err
+		}
+		archiveFile.Close()
+		getResp.Body.Close()
+
+		// Unzip the archive file.
+		unzipCmd := exec.Command("tar", "xvf", archiveFilename, "-C", ignoreFilesDir)
+		unzipCmd.Stdout = os.Stdout
+		if err = unzipCmd.Run(); err != nil {
+			return nil, err
+		}
+	}
+
+	// Walk the folder to get a map from name to file path.
+	nameToPath := make(map[string]string)
+	if err = filepath.Walk(ignoreFilesDir, func(path string, info os.FileInfo, err error) error {
+		filename := info.Name()
+		if endIndex := strings.Index(filename, ".gitignore"); endIndex != -1 {
+			name := strings.ToLower(filename[0:endIndex])
+			nameToPath[name] = path
+		}
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	return nameToPath, nil
+}
+
 func main() {
-	// Check arguments.
-	args := os.Args[1:]
-	if len(args) == 0 {
-		fmt.Println("Usage: got <type>...")
+	// Validate arguments.
+	args := os.Args
+	if len(args) <= 2 || args[1] != "generate" {
+		help()
+		return
+	}
+	requests := make(map[string]struct{})
+	for i := 2; i < len(args); i++ {
+		requests[strings.ToLower(args[i])] = struct{}{}
+	}
+
+	// Get a map from names to paths.
+	allIgnores, err := getIgnores()
+	if err != nil {
+		fmt.Println(err)
 		return
 	}
 
-	// Get all contents from gitignore.io
-	var gitignores string
-	for _, target := range args {
-		gitignore, err := getGitIgnore(target)
+	// Generate the .gitignore file.
+	var ignoreContent string
+	for request := range requests {
+		ignoreFile, ok := allIgnores[request]
+		if !ok {
+			fmt.Printf("%s cannot be recoginized\n", request)
+			return
+		}
+		var bytes []byte
+		bytes, err = ioutil.ReadFile(ignoreFile)
 		if err != nil {
 			fmt.Println(err)
-			os.Exit(0)
+			return
 		}
-		gitignores += gitignore
+		ignoreContent += string(bytes) + "\n"
 	}
 
-	// Create a .gitignore file.
-	gitIgnoreFile, err := os.Create(".gitignore")
-	if err != nil {
+	if err = ioutil.WriteFile(".gitignore", []byte(ignoreContent), 0700); err != nil {
 		fmt.Println(err)
-		os.Exit(0)
+		return
 	}
-	_, err = gitIgnoreFile.WriteString(gitignores)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(0)
-	}
-}
-
-func getGitIgnore(target string) (string, error) {
-	httpResponse, err := http.Get("https://www.gitignore.io/api/" + target)
-	if err != nil {
-		return "", err
-	}
-	defer httpResponse.Body.Close()
-	bytesContent, err := ioutil.ReadAll(httpResponse.Body)
-	if err != nil {
-		return "", err
-	}
-	content := string(bytesContent)
-	if strings.Contains(content, "ERROR") {
-		return "", fmt.Errorf("%s is undefined.", target)
-	}
-	return content, nil
 }
